f764a2794fb8bd103b92ce595c8b8361be1ff87f
Jeffrey Wang <jeffrey.yiji.wang@gmail.com>
2026-09-22 01:25:17 -0400

    feat:TM fuction

--- TM_examples.py
+++ TM_examples.py
@@ -9,12 +9,61 @@ def TM1():
     """Define and then return a Turing Machine with the following behavior:
 
     Input alphabet is {0, 1}. 
-    The tape alphabet is a superset of {0, 1, _}, where _ is the blank symbol.
+    The tape alphabet is a superset of {0, 1,X, _}, where _ is the blank symbol.
     (It is up to you if you want the tape alphabet to contain more symbols.)
     If the input has equal number of 0s and 1s, accept.
     Otherwise, reject.
+
+    we want to have the accepted states be we have all input written to X, and then 
+    then when we move to the left to the _ we accept
+    we reject, when we hit _ of the left or right but we have not 
+
+    we have to use the way we are collect the 1's and zero s
     """
-    pass
+    q_even = State("q_even")
+    q_extra_1 = State("q_extra_1")
+    q_extra_0 = State("q_extra_2")
+    q_left_to_start = State("q_left_to_start")
+    q_search_for_1 = State("q_search_for_1")
+    q_search_for_0 = State("q_search_for_0")
+    q_acc = State("q_acc")
+    q_rej = State("q_rej")
+    sigma = {'0','1'}
+    gamma = {'0', '1', 'X' , '_'}
+
+    q = {q_even,   q_extra_1,  q_extra_0,  q_left_to_start,  q_search_for_1,
+          q_search_for_0,q_acc, q_rej}
+
+    delta = {
+             ( q_even, 'X'): (q_even, 'X' , 'R'), 
+             ( q_even, '0'): (q_extra_0, 'X' , 'L'), 
+             ( q_even, '1'): (q_extra_1, 'X' , 'L'), 
+             ( q_even, '_'): (q_acc, '_' , 'L'), 
+             ( q_extra_0, '_'): (q_search_for_1, '_' , 'R'), 
+             ( q_extra_0, '0'): ( q_extra_0, '0' , 'L'), 
+             ( q_extra_0, '1'): ( q_extra_0, '1' , 'L'), 
+             ( q_extra_0, 'X'): ( q_extra_0, 'X' , 'L'),
+             ( q_extra_1, '_'): (q_search_for_0, '_' , 'R'), 
+             ( q_extra_1, '0'): (q_extra_1, '0' , 'L'), 
+             ( q_extra_1, '1'): (q_extra_1, '1' , 'L'), 
+             ( q_extra_1, 'X'): (q_extra_1, 'X' , 'L'),
+             ( q_search_for_1, '_'): (q_rej, '_' , 'L'),
+             ( q_search_for_1, '1'): (q_left_to_start, 'X' , 'L'),
+             ( q_search_for_1, '0'): (q_search_for_1, '0' , 'R'),
+             ( q_search_for_1, 'X'): ( q_search_for_1, 'X' , 'R'),
+            ( q_search_for_0, '_'): (q_rej, '_' , 'L'),
+            ( q_search_for_0, '1'): (q_left_to_start, '1' , 'R'),
+            ( q_search_for_0, '0'): (q_search_for_0, 'X' , 'L'),
+            ( q_search_for_0, 'X'): ( q_search_for_0, 'X' , 'R'),
+            ( q_left_to_start, '0'): ( q_left_to_start, '0' , 'L'),
+            ( q_left_to_start, '1'): ( q_left_to_start, '1' , 'L'),
+            ( q_left_to_start, 'X'): ( q_left_to_start, 'X' , 'L'),
+            ( q_left_to_start, '_'): ( q_even, '_' , 'R')
+             }
+
+    M = TuringMachine(q, sigma, gamma, delta, q_even, q_acc, q_rej)
+    return M
+
 
 def TM2():
     """Define and then return a Turing Machine with the following behavior:
@@ -26,11 +75,32 @@ def TM2():
     output the empty string.
     Otherwise, output x + 1 in binary, where x is the input number.
     """
-    pass
+        
+    q0 = State("q0")
+    q1 = State("q1")
+    q_acc = State("q_acc")
+    q_rej = State("q_rej")
+    sigma = {'0','1'}
+    gamma = {'0', '1', '_'}
+    q = {q0, q1, q_acc, q_rej}
+
+    delta = {(q0, '_'): (q_rej, '_' , 'R'), 
+                (q0, '0'): (q_acc, '1' , 'R'), 
+                (q0, '1'): (q1, '0' , 'R'), 
+                (q1, '0'): (q_acc, '1' , 'R'), 
+                (q1, '1'): (q1, '0' , 'R'),
+                (q1, '_'): (q_acc, '1' , 'R')  
+                }
+
+    M = TuringMachine(q, sigma, gamma, delta, q0, q_acc, q_rej)
+    
+    return M
+   
 
 def Bonus():
     """Define and then return a Turing Machine with the following behavior: (BONUS)
 
+    
     Input alphabet is {0, 1, $}.
     The tape alphabet is a superset of {0, 1, $, _}, where _ is the blank symbol.
     (It is up to you if you want the tape alphabet to contain more symbols.)
@@ -38,4 +108,4 @@ def Bonus():
     output the empty string.
     Otherwise, output the sum of x and y in binary.
     """
-    pass
+      
--- TM_interpreter.py
+++ TM_interpreter.py
@@ -22,10 +22,33 @@ def TM_interpret(M, x, k = None):
     if not validate_TM(M): return (None, Result.REJECT) # First, check that the input TM is a correct encoding.
 
     #TODO: What should the initial configuration be?
-    config = Configuration(None, None, None) 
-    
+    #the intial configuration should be the q0 to the left 
+    u = ""
+    q = M.q0
+    v = x
+    config = Configuration(u,q, v) 
+    config_list = [config]
+    index = 0
     while True:
+        if (isinstance(k, set)):
+            return (config_list, Result.UNDETERMINED)
+        elif(index == k):
+            return (config, Result.REJECT)
+
+
         config = simulate_step(M, config)
+        config_list.append(config)
+            
+        if(config.q == M.q_acc):
+            return (config, Result.ACCEPT)
+        elif(config.q == M.q_rej):
+            return (config, Result.REJECT)
+
+        index = index + 1 
+        
+
+        
+        
         # TODO fill in the rest of this loop
         # When do we know to Accept or Reject?
         # If k is set, when do we return "Undetermined after k steps"?
@@ -46,7 +69,38 @@ def validate_TM(M):
         - etc (determine the other things you should check)
     Return True if the input TM is a valid encoding, False otherwise.
     """
-    return False
+
+    if ((M.q0 not in M.Q )or( M.q_acc not in M.Q ) or (M.q_rej not in M.Q)):
+        return False
+    if(M.Gamma == {}):
+        return False
+    if(M.Sigma & M.Gamma) != M.Sigma:
+        return False
+    if(M.Sigma)== {}:
+        return False
+
+    """
+    check all delta to have valid states, in Q, and s that is in teh input alphebte
+    """
+    for config in M.delta:
+        new_state, new_tape_s, dir = config
+        if((new_state not in M.Q ) or (dir != 'L' ) or(dir != 'R')
+            or new_tape_s not in M.Sigma):
+           return False
+
+    for q in M.Q:
+        for tape_s in M.Sigma:
+            if((q,tape_s) not in M.delta):
+                return False
+            new_state, new_tape_s, dir = M.delta(q,tape_s)
+            if((new_state not in M.Q ) or (dir != 'L' ) or(dir != 'R')
+                or new_tape_s not in M.Sigma):
+                return False
+            if(dir == 'L' and q == M.q0):
+                return False
+            """ thknk about how going left on a q0 is false """
+         
+    return True
           
 def simulate_step(M, config):
     """TODO this is a helper function used by interpret to simulate one step of the TM.
@@ -54,10 +108,25 @@ def simulate_step(M, config):
     This is optional, but will be helpful in writing the TM interpreter.
 
     Given the current configuration, we want to compute and return the next configuration.
+
+
     """
     u, q, v = config.u, config.q, config.v # current configuration
-
+    s = v[0]
     # TODO compute the next configuration!
-    u_new, q_new, v_new = None, None, None
+    q_new,new_s, dir = M.delta(q, s)
+    ## we need to consider the empy case where you are at the end
+    if(dir == 'L'):
+        v = new_s+ v 
+        if(u != ""):
+            u = u[:-1]
+    else:
+        u = u + new_s
+        if(v != ""):
+            v = v[1:]
+        
+    
 
-    return Configuration(u_new, q_new, v_new) # return next configuration
+     
+
+    return Configuration(u, q_new, v) # return next configuration
